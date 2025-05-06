0764b648ab58c9dd9923f1f979c93c95d27d06c7
MoFunCode <mohamed.sul02@gmail.com>
2025-05-06 12:49:16 -0500

    implement employee time tracking with punchIn/punchOut methods

--- HotelOperations/src/main/java/com/hotel_exercise1/Employee.java
+++ HotelOperations/src/main/java/com/hotel_exercise1/Employee.java
@@ -1,5 +1,21 @@
 package com.hotel_exercise1;
 
+    /*
+    Employee
+
+    - Modify the Employee class to add a punchIn and punchOut methods. Each
+    time the employee punches in, we track their start time.
+   - When they punch out, we calculate how many hours they have worked and add
+    that time to their hours worked.
+    - To keep the math simple for now, each function will take a double as an input
+    argument.
+
+    punchIn(double time)
+    punchOut(double time
+    *
+    *
+    * */
+
 public class Employee {
 
     private int employeeId;
@@ -7,16 +23,38 @@ public class Employee {
     private String department;
     private double payRate;
     private double hoursWorked;
+    private double punchInTime;
+    private double checkInTime;
 
 
-    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+    public Employee(int employeeId, String name, String department, double payRate) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
-        this.hoursWorked = hoursWorked;
     }
 
+    public void checkIn(double time) {
+        if (checkInTime != 0) {
+            System.out.println("Already checked in!");
+            return;
+        }
+        checkInTime = time;
+    }
+
+    public void checkOut(double time) {
+        if (checkInTime == 0) {
+            System.out.println("Not checked in!");
+            return;
+        }
+
+        double hours = time - checkInTime;
+        if (hours < 0) {
+            hours += 24;
+        }
+
+        hoursWorked += hours;
+    }
 
     public int getEmployeeId() {
         return employeeId;
@@ -71,4 +109,5 @@ public class Employee {
         double overtimePay = getOvertimeHours() * (payRate * 1.5);
         return regularPay + overtimePay;
     }
+
 }
