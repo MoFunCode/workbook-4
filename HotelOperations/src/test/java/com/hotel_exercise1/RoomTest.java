2ca59a4aa37caf46930a4a98f0bfc83a13eaa2d7
MoFunCode <mohamed.sul02@gmail.com>
2025-05-07 15:08:33 -0500

    Add unit test for successful room checkout

--- HotelOperations/src/test/java/com/hotel_exercise1/RoomTest.java
+++ HotelOperations/src/test/java/com/hotel_exercise1/RoomTest.java
@@ -5,25 +5,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoomTest {
 
     @org.junit.jupiter.api.Test
-    void checkIn() throws Exception {
+    void testSuccessfulCheckIn() throws Exception {
+        // Arrange
+        Room room = new Room(1, 100, false, false);
 
-        Room room1 = new Room(1,150,false, false);
-        boolean result_For_Room1 = room1.checkIn();
+        // Act
+        boolean checkInResult = room.checkIn();
 
-        assertTrue(room1.isOccupied());
-        assertTrue(room1.isDirty());
-        assertFalse(result_For_Room1);
-
+        // Assert
+        assertTrue(checkInResult);
+        assertTrue(room.isOccupied());
+        assertTrue(room.isDirty());
 
     }
 
     @org.junit.jupiter.api.Test
-    void checkOut() {
+    void testSuccessfulCheckOut() {
+        Room occupiedRoom = new Room(1, 100, true, false);
+        boolean result = occupiedRoom.checkOut();
+
+        assertFalse(occupiedRoom.isOccupied());
+        assertTrue(occupiedRoom.isDirty());
+        assertTrue(result);
+
+
+
     }
 
-    @org.junit.jupiter.api.Test
-    void cleanRoom() {
 
-
-    }
 }
